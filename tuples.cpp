42c93875ce9be359d1aa2d8c62bdf897fabadaf3
Iliyan Gochev <iliyan.gochev@gmail.com>
2019-06-10 15:53:45 +0300

    Will be trying to keep as close to the books' naming scheme as possible

--- tuples.cpp
+++ tuples.cpp
@@ -4,7 +4,7 @@
 
 TEST_SUITE("Tuples") {
 	TEST_CASE("A tuple with w=1.0 is a point") {
-		auto a = create_tuple(4.3, -4.2, 3.1, 1.0);
+		auto a = Tuple(4.3, -4.2, 3.1, 1.0);
 		CHECK_EQ(a.x, doctest::Approx(4.3));
 		CHECK_EQ(a.y, doctest::Approx(-4.2));
 		CHECK_EQ(a.z, doctest::Approx(3.1));
@@ -14,7 +14,7 @@ TEST_SUITE("Tuples") {
 	}
 
 	TEST_CASE("A tuple with w=0.0 is a vector") {
-		auto a = create_tuple(4.3, -4.2, 3.1, 0.0);
+		auto a = Tuple(4.3, -4.2, 3.1, 0.0);
 		CHECK_EQ(a.x, doctest::Approx(4.3));
 		CHECK_EQ(a.y, doctest::Approx(-4.2));
 		CHECK_EQ(a.z, doctest::Approx(3.1));
@@ -23,42 +23,43 @@ TEST_SUITE("Tuples") {
 		CHECK_EQ(true, is_vector(a));
 	}
 
-	TEST_CASE("create_point() creates a tuple with w=1.0") {
-		auto p = create_point(4, -4, 3);
-		CHECK_EQ(p, create_tuple(4, -4, 3, 1));
+	TEST_CASE("point() creates a tuple with w=1.0") {
+		auto p = point(4, -4, 3);
+		CHECK_EQ(p, Tuple(4, -4, 3, 1));
 	}
 
-	TEST_CASE("create_vector() creates a tuple with w=0.0") {
-		auto v = create_vector(4, -4, 3);
-		CHECK_EQ(v, create_tuple(4, -4, 3, 0));
+	TEST_CASE("vector() creates a tuple with w=0.0") {
+		auto v = vector(4, -4, 3);
+		CHECK_EQ(v, Tuple(4, -4, 3, 0));
 	}
 
 	TEST_CASE("Adding two tuples") {
-		auto a1 = create_tuple(3, -2, 5, 1);
-		auto a2 = create_tuple(-2, 3, 1, 0);
-		CHECK_EQ(a1 + a2, create_tuple(1, 1, 6, 1));
+		auto a1 = Tuple(3, -2, 5, 1);
+		auto a2 = Tuple(-2, 3, 1, 0);
+		CHECK_EQ(a1 + a2, Tuple(1, 1, 6, 1));
 	}
 
 	TEST_CASE("Subtracting two points") {
-		auto p1 = create_point(3, 2, 1);
-		auto p2 = create_point(5, 6, 7);
-		CHECK_EQ(p1 - p2, create_vector(-2, -4, -6));
+		auto p1 = point(3, 2, 1);
+		auto p2 = point(5, 6, 7);
+		CHECK_EQ(p1 - p2, vector(-2, -4, -6));
 	}
 
 	TEST_CASE("Subtracting vector from a point") {
-		auto p = create_point(3, 2, 1);
-		auto v = create_vector(5, 6, 7);
-		CHECK_EQ(p - v, create_point(-2, -4, -6));
+		auto p = point(3, 2, 1);
+		auto v = vector(5, 6, 7);
+		CHECK_EQ(p - v, point(-2, -4, -6));
 	}
 
 	TEST_CASE("Subtracting two vectors") {
-		auto v1 = create_vector(3, 2, 1);
-		auto v2 = create_vector(5, 6, 7);
-		CHECK_EQ(v1 - v2, create_vector(-2, -4, -6));
+		auto v1 = vector(3, 2, 1);
+		auto v2 = vector(5, 6, 7);
+		CHECK_EQ(v1 - v2, vector(-2, -4, -6));
 	}
 
 	TEST_CASE("Negating a tuple") {
-		auto a = create_tuple(1, -2, 3, -4);
-		CHECK_EQ(-a, create_tuple(-1, 2, -3, 4));
+		auto a = Tuple(1, -2, 3, -4);
+		CHECK_EQ(-a, Tuple(-1, 2, -3, 4));
 	}
+
 }
--- tuples.h
+++ tuples.h
@@ -4,7 +4,7 @@ struct tuple {
 	real x, y, z, w;
 };
 
-inline tuple create_tuple(real x, real y, real z, real w) {
+inline tuple Tuple(real x, real y, real z, real w) {
 	tuple result = { 
 		.x =x,
 		.y =y,
@@ -13,12 +13,12 @@ inline tuple create_tuple(real x, real y, real z, real w) {
 	return result;
 }
 
-inline tuple create_point(real x, real y, real z) {
-	return create_tuple(x, y, z, 1.0);
+inline tuple point(real x, real y, real z) {
+	return Tuple(x, y, z, 1.0);
 }
 
-inline tuple create_vector(real x, real y, real z) {
-	return create_tuple(x, y, z, 0.0);
+inline tuple vector(real x, real y, real z) {
+	return Tuple(x, y, z, 0.0);
 }
 
 inline bool is_point(tuple p) { return p.w == 1.0; }
