608df7d8983f17095170cbc1b982d35a640ee568
Iliyan Gochev <iliyan.gochev@gmail.com>
2019-06-10 14:35:44 +0300

    Initial commit of the project

---
+++ TheRayTracerChallenge.h
@@ -0,0 +1,9 @@
+﻿// TheRayTracingChallenge.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "doctest.h"
+
+using real = float;
