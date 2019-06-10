906ec856c29a2524947987c683d8cfc07adf27f0
Iliyan Gochev <iliyan.gochev@gmail.com>
2019-06-10 14:51:14 +0300

    Inline all the tuple functions, add operator==

--- tuple.h
+++ tuple.h
@@ -4,18 +4,22 @@ struct tuple {
 	real x, y, z, w;
 };
 
-tuple create_tuple(real x, real y, real z, real w) {
+inline tuple create_tuple(real x, real y, real z, real w) {
 	tuple result = { x,y,z,w };
 	return result;
 }
 
-tuple create_point(real x, real y, real z) {
+inline tuple create_point(real x, real y, real z) {
 	return create_tuple(x, y, z, 1);
 }
 
-tuple create_vector(real x, real y, real z) {
+inline tuple create_vector(real x, real y, real z) {
 	return create_tuple(x, y, z, 1.0);
 }
 
-bool is_point(tuple p) { return p.w == 1.0; }
-bool is_vector(tuple v) { return v.w == 0.0; }
+inline bool is_point(tuple p) { return p.w == 1.0; }
+inline bool is_vector(tuple v) { return v.w == 0.0; }
+
+inline bool operator==(tuple const& l, tuple const& r) {
+	return (l.x == r.x) && (l.y == r.y) && (l.z == r.z) && (l.w == r.w);
+}
